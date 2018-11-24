af7cf67af95bdaa90b07658fc982f1ccba9700ac
msevilla <m.sevillazhou@gmail.com>
2018-11-24 15:00:03 +0100

    Added images and sound playback

--- 1click.js
+++ 1click.js
@@ -1,4 +1,27 @@
-var audio = $("#mySoundClip")[0];
-$("nav a").mouseenter(function() {
-  audio.play();
-});
+var coffee_sound = new Audio ();
+coffee_sound.src = "audio/coffee.m4a";
+
+// function playSound(soundobj) {
+//   var thissound=document.getElementById(soundobj);
+//   thissound.play();
+// }
+
+// function StopSound(soundobj) {
+//   var thissound=document.getElementById(soundobj);
+//   thissound.pause();
+//   thissound.currentTime = 0;
+// }
+
+// var item = document.getElementById("img");
+// var music = new Audio('audio/coffee.m4a');
+
+// item.addEventListener("mouseover", playMusic, false);
+// item.addEventListener("mouseout", stopMusic, false);
+
+// function playMusic() {
+//    music.play();
+// }
+
+// function stopMusic() {
+//    music.stop();
+// }
